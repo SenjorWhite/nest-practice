a52452b4aa36dca5847e2868b1364b7830399fa3
senjorwhite <senjorwhite@gmail.com>
2021-09-20 15:45:45 -0600

    feat(messages): added basic dto into the stack

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { createMessageDto } from './dtos/create-messages.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -6,7 +7,7 @@ export class MessagesController {
   listMessages() {}
 
   @Post()
-  createMessages(@Body() body: any) {
+  createMessages(@Body() body: createMessageDto) {
     console.log(body);
   }
 
